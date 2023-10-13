6fd7e5b26f12976af1a5cd08dd002523fa116c9b
LudwigPauly <ludwigpauly@gmail.com>
2023-10-13 17:04:45 +0200

    Add param to SteadyStateProbs

--- prism/src/prism/SteadyStateProbs.java
+++ prism/src/prism/SteadyStateProbs.java
@@ -14,6 +14,9 @@ import explicit.*;
 import explicit.SCCComputer;
 import jdd.JDD;
 import jdd.JDDNode;
+import param.Function;
+import param.ParamModel;
+import param.ParamModelChecker;
 
 /**
  * Storage for the steady-state probabilities for each BSCC of a model.
@@ -57,6 +60,37 @@ public interface SteadyStateProbs<S, V>
 			numberOfBSCCs  = 0;
 			numberOfStates = 0;
 			bsccProbs = new double[0];
+			return this;
+		}
+
+		@Override
+		public BitSet getNonBsccStates()
+		{
+			BitSet nonBsccStates = getBsccStates();
+			nonBsccStates.flip(0, numberOfStates);
+			return nonBsccStates;
+		}
+	}
+
+	public abstract class SteadyStateProbsParam implements SteadyStateProbs<BitSet, Function[]>
+	{
+		protected int numberOfBSCCs;
+		protected int numberOfStates;
+		protected Function[] bsccProbs;  // steady-state probabilities for all BSCCs
+
+		public SteadyStateProbsParam(ParamModel model, Function[] bsccProbs)
+		{
+			this.bsccProbs = bsccProbs;
+			numberOfBSCCs  = 0;
+			numberOfStates = model.getNumStates();
+		}
+
+		@Override
+		public SteadyStateProbsParam clear()
+		{
+			numberOfBSCCs  = 0;
+			numberOfStates = 0;
+			bsccProbs = new Function[0];
 			return this;
 		}
 
@@ -141,6 +175,80 @@ public interface SteadyStateProbs<S, V>
 		}
 	}
 
+	public static class SteadyStateProbsCompactParam extends SteadyStateProbsParam
+	{
+
+		protected int[] bsccIndices;   // states of all BSCCs, descending order
+		protected int[] bsccOffsets;   // offsets bscc i: bsccOffsets[i], [bsccOffsets[i+1]
+
+		public SteadyStateProbsCompactParam(ParamModel model, Function[] bsccProbs)
+		{
+			super(model, bsccProbs);
+			bsccIndices   = new int[numberOfStates];
+			bsccOffsets   = new int[numberOfStates + 1];
+		}
+
+		@Override
+		public BitSet addBSCC(BitSet bscc)
+		{
+			int lower = bsccOffsets[numberOfBSCCs];
+			int upper = lower + bscc.cardinality();
+			numberOfBSCCs++;
+			bsccOffsets[numberOfBSCCs] = upper;
+			int state = -1;
+			// copy bscc in decending order
+			for (int i = upper - 1; i >= lower; i--) {
+				state = bscc.nextSetBit(state + 1);
+				bsccIndices[i] = state;
+			}
+			return bscc;
+		}
+
+		@Override
+		public SteadyStateProbsCompactParam clear()
+		{
+			bsccIndices = new int[0];
+			bsccOffsets = new int[1];
+			return this;
+		}
+
+		@Override
+		public FunctionalIterable<BitSet> getBSCCs()
+		{
+			Range indices = new Range(numberOfBSCCs);
+			FunctionalIterable<OfInt> bsccs = indices.map((int i) -> new IterableArray.OfInt(bsccIndices, bsccOffsets[i], bsccOffsets[i+1]));
+			return bsccs.map(bscc -> bscc.collect(new BitSet()));
+		}
+
+		@Override
+		public BitSet getBsccStates()
+		{
+			return new IterableArray.OfInt(bsccIndices).collect(new BitSet(numberOfStates));
+		}
+
+		@Override
+		public int getNumberOfBSCCs()
+		{
+			return numberOfBSCCs;
+		}
+
+		@Override
+		public Function[] getSteadyStateProbabilities()
+		{
+			return bsccProbs;
+		}
+
+		@Override
+		public SteadyStateProbsCompactParam trimToSize()
+		{
+			bsccIndices = Arrays.copyOf(bsccIndices, bsccOffsets[numberOfBSCCs]);
+			bsccOffsets = Arrays.copyOf(bsccOffsets, numberOfBSCCs + 1);
+			return this;
+		}
+	}
+
+
+
 	public static class SteadyStateProbsSimple extends SteadyStateProbsExplicit
 	{
 		protected ArrayList<BitSet> bsccs;  // list of BSCCs
@@ -197,6 +305,68 @@ public interface SteadyStateProbs<S, V>
 
 		@Override
 		public SteadyStateProbsSimple trimToSize()
+		{
+			bsccs.trimToSize();
+			return this;
+		}
+	}
+
+	public static class SteadyStateProbsSimpleParam extends SteadyStateProbsParam
+	{
+		protected ArrayList<BitSet> bsccs;  // list of BSCCs
+
+		public SteadyStateProbsSimpleParam(ParamModel model, Function[] bsccProbs)
+		{
+			super(model, bsccProbs);
+			bsccs = new ArrayList<>();
+		}
+
+		@Override
+		public BitSet addBSCC(BitSet bscc)
+		{
+			numberOfBSCCs++;
+			bsccs.add(bscc);
+			return bscc;
+		}
+
+		@Override
+		public SteadyStateProbsSimpleParam clear()
+		{
+			super.clear();
+			bsccs.clear();
+			return this;
+		}
+
+		@Override
+		public BitSet getBsccStates()
+		{
+			BitSet bsccStates = new BitSet(numberOfStates);
+			for (BitSet bscc : bsccs) {
+				bsccStates.or(bscc);
+			}
+			return bsccStates;
+		}
+
+		@Override
+		public int getNumberOfBSCCs()
+		{
+			return numberOfBSCCs;
+		}
+
+		@Override
+		public Function[] getSteadyStateProbabilities()
+		{
+			return bsccProbs;
+		}
+
+		@Override
+		public FunctionalIterable<BitSet> getBSCCs()
+		{
+			return Reducible.extend(bsccs);
+		}
+
+		@Override
+		public SteadyStateProbsSimpleParam trimToSize()
 		{
 			bsccs.trimToSize();
 			return this;
@@ -316,10 +486,22 @@ public interface SteadyStateProbs<S, V>
 		double[] probs = new double[dtmc.getNumStates()];
 		return compute(mc, dtmc, new SteadyStateProbsCompact(dtmc, probs));
 	}
+	public static SteadyStateProbsCompactParam computeCompactParam(ParamModelChecker mc, ParamModel model) throws PrismException
+	{
+		Function[] probs = new Function[model.getNumStates()];
+		return computeParam(mc, model, new SteadyStateProbsCompactParam(model, probs));
+	}
+
 	public static SteadyStateProbsSimple computeSimple(MCModelChecker mc, DTMC dtmc) throws PrismException
 	{
 		double[] probs = new double[dtmc.getNumStates()];
 		return compute(mc, dtmc, new SteadyStateProbsSimple(dtmc, probs));
+	}
+
+	public static SteadyStateProbsSimpleParam computeSimpleParam(ParamModelChecker mc, ParamModel model) throws PrismException
+	{
+		Function[] probs = new Function[model.getNumStates()];
+		return computeParam(mc, model, new SteadyStateProbsSimpleParam(model, probs));
 	}
 
 	@SuppressWarnings("unchecked")
@@ -343,7 +525,33 @@ public interface SteadyStateProbs<S, V>
 
 		return (T) steadyStateProbs.trimToSize();
 	}
-	
+
+	public static <T extends SteadyStateProbsParam> T computeParam(ParamModelChecker mc, ParamModel model, T steadyStateProbs) throws PrismException
+	{
+		// Compute BSCCs
+		SCCConsumerStore sccStore = new SCCConsumerStore();
+		explicit.SCCComputer sccComputer = explicit.SCCComputer.createSCCComputer((PrismComponent) mc, model, sccStore);
+		sccComputer.computeSCCs();
+		List<BitSet> bsccs = sccStore.getBSCCs();
+		int numBSCCs = bsccs.size();
+
+
+		// Compute steady-state probabilities for each BSCC
+		for (int b = 0; b < numBSCCs; b++) {
+			BitSet bscc = bsccs.get(b);
+			// Compute steady-state probabilities for the BSCC
+			Function[] tmp = mc.computeSteadyStateProbsForBSCC(model, bscc);
+			int k = 0;
+			for (int i = bscc.nextSetBit(0); i >= 0; i = bscc.nextSetBit(i + 1)) {
+				steadyStateProbs.bsccProbs[i] = tmp[k];
+				k++;
+			}
+			steadyStateProbs.addBSCC(bscc);
+		}
+
+		return (T) steadyStateProbs.trimToSize();
+	}
+
 	public static SteadyStateProbsSymbolic computeSymbolic(ProbModelChecker mc) throws PrismException
 	{
 		prism.StateValues probs = mc.createVector(0);
